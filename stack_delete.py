a1fcfa82f1889471b19acf2b4d3bdf148dde26c4
Bisi Kuku <bisi.kuku@teladochealth.com>
2022-05-13 08:56:23 -0400

    adding script to repo

---
+++ stack_delete.py
@@ -0,0 +1,45 @@
+import boto3
+import functools
+import re
+
+###### Info about code ##########
+# Basic assumption here is that you have permissions to access the AWS resources.
+# You can use the AWS CLI to check the permissions.
+# You can use the AWS CloudFormation console to check the stack status.
+# boto3 is used to access the AWS resources and needs to be installed.
+# This small snippet is used to get the list of specific stacks and delete them.
+# Please be free to use it in your own projects and feel free to modify it, improve it and share it. 
+#################################
+
+# Get the list of activated or already 'opt in' regions.
+def get_region_names():
+    ec2_client = boto3.client('ec2')
+    response = ec2_client.describe_regions()
+    regions = response['Regions']
+    return [region['RegionName'] for region in regions] 
+
+regions = get_region_names()
+
+# Get the list of stacks in all regions and their corresponding stacts and delete them.
+for region in regions:
+    client = boto3.client('cloudformation', region_name=region)
+    paginator = client.get_paginator('list_stacks')
+    status_filter = ['CREATE_COMPLETE', 'UPDATE_COMPLETE', 'ROLLBACK_COMPLETE', 'CREATE_IN_PROGRESS', 'UPDATE_IN_PROGRESS', 'ROLLBACK_IN_PROGRESS']
+
+    response_iterator = paginator.paginate(
+    StackStatusFilter=status_filter
+    )
+    stacks = []
+    for response in response_iterator:
+        stacks += [stack['StackName'] for stack in response['StackSummaries']]
+    # adding this to get the stacks that have the string 'pattern' in them, this is a hacky way to get the stacks that we want to delete
+    def get_stacksList():
+        return [st for st in stacks if any(sub in st for sub in ['partern1', 'partern2', 'partern3'])]
+    stacks = (get_stacksList())
+    for stack in stacks:
+        print(stack)
+        client = boto3.client('cloudformation' , region_name=region)
+        client.delete_stack(StackName=stack)
+
+
+
